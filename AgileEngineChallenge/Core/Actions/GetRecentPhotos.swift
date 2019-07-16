cca173742d378f20809a09a8c6f2dd9b39619406
Daniel Mankowski <daniel.mankowski@gmail.com>
2019-07-16 00:26:52 -0300

    get recent photos action

---
+++ AgileEngineChallenge/Core/Actions/GetRecentPhotos.swift
@@ -0,0 +1,9 @@
+//
+//  GetRecentPhotos.swift
+//  AgileEngineChallenge
+//
+//  Created by Daniel Mankowski on 15/07/2019.
+//  Copyright © 2019 Daniel Mankowski. All rights reserved.
+//
+
+import Foundation
